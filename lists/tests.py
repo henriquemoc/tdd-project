be80e219cf97f68ca1883e1678fb977ee44f44a7
Henrique Magalhães <55369113+henriquemoc@users.noreply.github.com>
2023-05-04 01:20:05 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 2

--- lists/tests.py
+++ lists/tests.py
@@ -6,15 +6,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
@@ -51,3 +42,14 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
         self.assertEqual(second_saved_item.text, 'O segundo item')
+
+class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
