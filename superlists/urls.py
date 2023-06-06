07e4d250ec515e4fa5abaaf53628e304cdda80b6
Henrique Magalhães <55369113+henriquemoc@users.noreply.github.com>
2023-06-06 15:49:16 -0300

    Modelo de usuário mínimo

--- superlists/urls.py
+++ superlists/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/new$', list_views.new_list, name='new_list'),
     url(r'^lists/(\d+)/$', list_views.view_list, name='view_list'),
-
-    url(r'^accounts/send_email$', accounts_views.send_login_email, name='send_login_email'),
-    url(r'^accounts/login$', accounts_views.login, name='login'),
-    url(r'^accounts/logout$', accounts_views.logout, name='logout'),
 ]
